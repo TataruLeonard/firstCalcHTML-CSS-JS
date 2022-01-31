8e7ba58e78c3885116f9e154af9ed240fc3a355c
n3where <n3where@yahoo.com>
2022-01-31 21:36:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstCalc';
+
+  num: number = null 
+  numberOne: number = 0
+  action: string = ""
+
+  add(){
+    this.numberOne = this.num
+    this.num = null
+    this.action = "+"
+  }
+
+  minus(){
+    this.numberOne = this.num
+    this.num = null
+    this.action = "-"
+  }
+
+  divided(){
+    this.numberOne = this.num
+    this.num = null
+    this.action = "/"
+  }
+
+  multiply(){
+    this.numberOne = this.num
+    this.num = null
+    this.action = "*"
+  }
+
+  delete(){
+    this.num = null
+    this.numberOne = null
+    this.action = ""
+  }
+
+  equal(){
+    switch(this.action){
+      case "+":this.num += this.numberOne;break;
+      case "-":this.num = this.numberOne - this.num;break;
+      case "*":this.num *= this.numberOne;break;
+      case "/":this.num = this.numberOne/this.num;break;
+    }
+
+  }
+
+  buttonPress(event: any){
+    switch(event){
+      case 107:this.add();break;
+      case 109:this.minus();break;
+      case 106:this.multiply();break;
+      case 111:this.divided();break;
+      case 13:this.equal();break;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
